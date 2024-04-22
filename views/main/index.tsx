53484e29d6f8246fb77817fa29b0fe632d1cb164
git-marcopitra <marcomorete73@hotmail.com>
2024-04-22 10:13:42 +0100

    :sparkles: feat: add draggable element

--- views/main/index.tsx
+++ views/main/index.tsx
@@ -1,10 +1,25 @@
 import { Div, H1 } from '@stylin.js/elements';
-import { FC } from 'react';
+import { motion } from 'framer-motion';
+import { FC, useRef } from 'react';
 
-const Main: FC = () => (
-  <Div>
-    <H1>Main</H1>
-  </Div>
-);
+const MotionDiv = motion(Div);
+
+const Main: FC = () => {
+  const constraintsRef = useRef(null);
+
+  return (
+    <MotionDiv minHeight="100vh" minWidth="100vw" ref={constraintsRef}>
+      <H1>Main</H1>
+      <MotionDiv
+        drag
+        bg="red"
+        width="10rem"
+        height="7rem"
+        borderRadius="1rem"
+        dragConstraints={constraintsRef}
+      />
+    </MotionDiv>
+  );
+};
 
 export default Main;
